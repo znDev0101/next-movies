018f7a3fb6c478aae8844d89f942240d85566ff5
znDev0101 <zulfanurhuda01@gmail.com>
2024-11-14 07:29:35 +0700

    optimized

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import MainNav from "@/components/layouts/MainNav";
 import { Providers } from "./providers";
