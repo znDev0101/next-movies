b6da710f8f4f780adbfeb5e909241a49de2ad437
znDev0101 <zulfanurhuda01@gmail.com>
2024-11-14 11:49:13 +0700

    fix error api call

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,16 +9,15 @@ const HeroSection = async () => {
   );
 
   const results = await response.json();
-
-  const randomIndex = Math.floor(Math.random() * 20);
-  const data = await results.results?.slice(randomIndex, randomIndex + 1);
+  const randomItem =
+    results.results[Math.floor(Math.random() * results.results.length)];
 
   return (
     <section className='max-w-[81rem] mx-auto h-screen relative mt-3 rounded-lg overflow-hidden'>
       <div className='h-full'>
         <div className='relative w-full h-full'>
           <Image
-            src={`${pathImg}${data[0]?.backdrop_path}`}
+            src={`${pathImg}${randomItem?.backdrop_path}`}
             alt='hero movie img'
             fill
             style={{
@@ -31,13 +30,10 @@ const HeroSection = async () => {
           <div className='w-max text-center mx-auto px-2 py-1 text-white rounded-full   bg-black dark:bg-white'>
             <p className='text-white dark:text-black'>Trending Now</p>
           </div>
-          <h1>Test</h1>
-          <div className='text-foreground flex flex-col gap-y-8'>
-            <h1 className='text-6xl font-bold'>
-              {data[0]?.title || data[0]?.name}
-            </h1>
-            <p className='font-semibold text-gray-600'>{results?.overview}</p>
-          </div>
+          <h1 className='text-6xl font-bold'>
+            {randomItem?.title || randomItem?.name}
+          </h1>
+          <p className='font-semibold text-gray-600'>{results?.overview}</p>
         </div>
         <div className='absolute w-full h-full top-0 bottom-0 left-0 right-0 bg-gradient-to-t from-white from-5% opacity-60 dark:from-black dark:from-20% dark:opacity-85 z-20'></div>
       </div>
