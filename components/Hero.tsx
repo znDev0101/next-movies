3245915ff3dea90dd8a15df795df1ac9d196634d
znDev0101 <zulfanurhuda01@gmail.com>
2024-11-14 07:50:46 +0700

    add title and overview in hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,7 +19,7 @@ const HeroSection = async () => {
 
   // const randomIndex = Math.floor(Math.random() * results.results?.length);
   // const data = results.results?.slice(randomIndex, randomIndex + 1);
-
+  console.log(results);
   return (
     <section className='max-w-[81rem] mx-auto h-screen relative mt-3 rounded-lg overflow-hidden'>
       <div className='h-full'>
@@ -34,18 +34,19 @@ const HeroSection = async () => {
             priority
           />
         </div>
-        {/* <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 translate-y-1/4 text-center flex flex-col lg:gap-y-2 pt-20 z-30'>
+        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 translate-y-1/4 text-center flex flex-col lg:gap-y-2 pt-20 z-30'>
           <div className='w-max text-center mx-auto px-2 py-1 text-white rounded-full   bg-black dark:bg-white'>
             <p className='text-white dark:text-black'>Trending Now</p>
           </div>
 
           <div className='text-foreground flex flex-col gap-y-8'>
             <h1 className='text-6xl font-bold'>
-              {data?.[0].title || data?.[0].name}
+              {/* {data?.[0].title || data?.[0].name} */}
+              {results?.original_title}
             </h1>
-            <p className='font-semibold text-gray-600'>{data?.[0].overview}</p>
+            <p className='font-semibold text-gray-600'>{results?.overview}</p>
           </div>
-        </div> */}
+        </div>
         <div className='absolute w-full h-full top-0 bottom-0 left-0 right-0 bg-gradient-to-t from-white from-5% opacity-60 dark:from-black dark:from-20% dark:opacity-85 z-20'></div>
       </div>
     </section>
