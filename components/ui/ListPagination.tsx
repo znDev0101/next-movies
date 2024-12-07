0ceb53b9e943fc72567616c4a92db28dac4208b2
znDev0101 <zulfanurhuda01@gmail.com>
2024-12-07 20:41:36 +0700

    try fix error

--- components/ui/ListPagination.tsx
+++ components/ui/ListPagination.tsx
@@ -2,7 +2,7 @@
 
 import { Pagination } from "@nextui-org/pagination";
 import { useRouter, useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const ListPagination = () => {
   const searchParams = useSearchParams();
@@ -10,18 +10,18 @@ const ListPagination = () => {
 
   const totalPages = 100;
   const router = useRouter();
-  const [siblings, setSiblings] = useState<number>(1);
+  // const [siblings, setSiblings] = useState<number>(1);
 
-  useEffect(() => {
-    const updateSiblings = () => {
-      setSiblings(window.innerWidth < 428 ? 0 : 1);
-    };
+  // useEffect(() => {
+  //   const updateSiblings = () => {
+  //     setSiblings(window.innerWidth < 428 ? 0 : 1);
+  //   };
 
-    updateSiblings();
-    window.addEventListener("resize", updateSiblings);
+  //   updateSiblings();
+  //   window.addEventListener("resize", updateSiblings);
 
-    return () => window.removeEventListener("resize", updateSiblings); // Clean up
-  }, []);
+  //   return () => window.removeEventListener("resize", updateSiblings); // Clean up
+  // }, []);
 
   const handlePageChange = (page: number) => {
     router.push(`/movie/popular?page=${page}`);
