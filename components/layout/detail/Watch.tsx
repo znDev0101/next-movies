fa9a491a2cde19553312cf13fb3166b71b849546
znDev0101 <zulfanurhuda01@gmail.com>
2024-12-10 20:29:02 +0700

    try fix error

--- components/layout/detail/Watch.tsx
+++ components/layout/detail/Watch.tsx
@@ -1,14 +1,41 @@
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
+type Provider = {
+  link: string;
+  flatrate?: Array<{
+    display_priority: number;
+    logo_path: string;
+    provider_name: string;
+    provider_id: number;
+  }>;
+  buy?: Array<{
+    display_priority: number;
+    logo_path: string;
+    provider_name: string;
+    provider_id: number;
+  }>;
+  rent?: Array<{
+    display_priority: number;
+    logo_path: string;
+    provider_name: string;
+    provider_id: number;
+  }>;
+};
+
+type WatchProviders = {
+  [countryCode: string]: Provider;
+};
+
 const Watch = ({ mediatype, id }: { mediatype: string; id: string }) => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<WatchProviders | null>(null);
 
   const getDataWatchProvider = async () => {
     try {
       const response = await fetch(
         `https://api.themoviedb.org/3/${mediatype}/${id}/watch/providers?api_key=${process.env.API_KEY}`,
       );
-      const results = await response.json();
+      const { results }: { results: WatchProviders } = await response.json();
       setData(results);
     } catch (error) {
       console.log(error);
@@ -21,15 +48,54 @@ const Watch = ({ mediatype, id }: { mediatype: string; id: string }) => {
 
   return (
     <div>
-      <h1>Watch</h1>
-      <p>
-        Hello Test Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-        Officiis sint neque quibusdam hic corporis ut nemo pariatur impedit
-        eaque veniam. Nemo optio placeat sapiente sed dolore corrupti
-        accusantium a quod assumenda. Incidunt cupiditate accusantium aliquam,
-        accusamus, non maiores, deleniti delectus voluptate ratione
-        exercitationem quos. Sequi ex earum explicabo similique eveniet?
-      </p>
+      <h1>Where to Watch</h1>
+      <p>Stream, buy or rent this movie from the providers below.</p>
+      <div className="my-6 flex flex-col gap-y-6 lg:flex-row lg:gap-x-80">
+        <div className="flex-col">
+          <h1 className="font-semibold">test</h1>
+          <p>Not Available</p>
+        </div>
+        <div>
+          <h1 className="font-semibold">Buy</h1>
+          {data?.US.buy?.map((data, i) => {
+            return (
+              <div className="my-4 flex items-center gap-x-3" key={i}>
+                <div className="relative h-10 w-10">
+                  <Image
+                    src={`${process.env.NEXT_PUBLIC_IMG_PATH}/original${data.logo_path}`}
+                    alt="logo image"
+                    fill
+                    style={{
+                      objectFit: "cover",
+                    }}
+                  />
+                </div>
+                <h1>{data.provider_name}</h1>
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex flex-col justify-center">
+          <h1 className="font-semibold">Rent</h1>
+          {data?.US.rent?.map((data, i) => {
+            return (
+              <div className="my-4 flex items-center gap-x-3" key={i}>
+                <div className="relative h-10 w-10">
+                  <Image
+                    src={`${process.env.NEXT_PUBLIC_IMG_PATH}/original${data.logo_path}`}
+                    alt="logo image"
+                    fill
+                    style={{
+                      objectFit: "cover",
+                    }}
+                  />
+                </div>
+                <h1>{data.provider_name}</h1>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
--- components/ui/TabsDetails.tsx
+++ components/ui/TabsDetails.tsx
@@ -6,39 +6,8 @@ import Credits from "../layout/detail/Credits";
 import Watch from "../layout/detail/Watch";
 
 const TabsDetails = ({ id, mediatype }: { id: string; mediatype: string }) => {
-  const tabs = [
-    {
-      id: "credits",
-      label: "Credits",
-    },
-    {
-      id: "watch",
-      label: "Watch",
-    },
-    {
-      id: "reviews",
-      label: "Reviews",
-    },
-    {
-      id: "images",
-      label: "Images",
-    },
-    {
-      id: "videos",
-      label: "Videos",
-    },
-    {
-      id: "recommendations",
-      label: "Recommendations",
-    },
-    {
-      id: "similiar",
-      label: "Similiar",
-    },
-  ];
-
   return (
-    <section className="mx-auto lg:w-[85%]">
+    <section className="mx-auto w-[96%] lg:w-[85%]">
       <div className="flex w-full flex-col">
         <div className="flex w-full flex-col">
           <Tabs aria-label="Options">
